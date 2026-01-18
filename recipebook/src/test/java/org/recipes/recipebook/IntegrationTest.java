6bb4cd1b57f39905390afe627817966f00b607bb
Jessica Pamdeth <jpamdeth@gmail.com>
2026-01-17 20:55:14 -0500

    Bump version and cleanup imports

--- recipebook/src/test/java/org/recipes/recipebook/IntegrationTest.java
+++ recipebook/src/test/java/org/recipes/recipebook/IntegrationTest.java
@@ -11,9 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.recipes.recipebook.helper.TestObjects;
 import org.recipes.recipebook.model.Recipe;
-import org.recipes.recipebook.model.RecipeIngredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
